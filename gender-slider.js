a1f482bed0659cf6736942a26f089c0afa3e1e84
EverlessDrop41 <tomfperegrine@googlemail.com>
2015-07-19 18:53:57 +0100

    Added Genderfluid and Unsure options

--- gender-slider.js
+++ gender-slider.js
@@ -1,16 +1,47 @@
 var slider = document.querySelector('#gender-slider');
 var msgOutput = document.getElementById('gender-string');
 
+// Gender Values
+// < 0 - Genderfluid
+// 0 - 3.444.. - Female
+// 3.5 - 6.444... - Agender
+// 6.5 - 10 - Male
+// > 10 - Unsure/Not Say
+
 var FeminineMsg = "Female";
 var MasculineMsg = "Male"; 
 var NeutralMsg = "Agender";
 var FluidMsg = "Gender Fluid";
 var UnsureMsg = "Unsure";
 
+var GenderValue = 5;
+var InputType = "Slider";
+
+function typeChange (val) {
+	//TODO: code
+	InputType = val;
+
+	if (InputType == "Slider") {
+		GenderValue = slider.value;
+		updateOutput(GenderValue, '#gender-slider-out');
+	} else if (InputType == "Fluid") {
+		GenderValue = 11;
+	}
+	else {
+		GenderValue = -1;
+	}
+	
+	updateMsg(GenderValue);
+}
+
 function updateOutput (val, outputId) {
+	console.log(GenderValue);
 	var output = document.querySelector(outputId);
 	output.value = val;
-	updateMsg(val);
+	if (InputType == "Slider") {
+		GenderValue = val;
+	}
+	updateMsg(GenderValue);
 }
 
 function updateMsg (val) {
@@ -25,12 +56,12 @@ function updateMsg (val) {
 	else if (val >= 6.5 && val <= 10) {
 		outputMsg = MasculineMsg;
 	}
-	else if (val < 0) {
+	else if (val > 10) {
 		outputMsg = FluidMsg;
 	}
 	else {
 		outputMsg = UnsureMsg;
 	}
 	msgOutput.innerHTML = outputMsg;
-	console.log(msgOutput.text);
+	console.log(outputMsg);
 }
