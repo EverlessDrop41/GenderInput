ee2cb8cc39cbf372b700d9f0be8370403a155689
Emily Peregrine <emilyperegrine@Emilys-MBP.home>
2015-09-03 20:46:31 +0100

    Added pronoun class

---
+++ scripts/classes/pronoun.js
@@ -0,0 +1,8 @@
+//Created by Tom Peregrine 03/09/2015 DD/MM/YYYY
+function Pronoun (nominative, objective, determiner, pronoun, reflexive) {
+	this.Nominative = nominative;
+	this.Objective = objective;
+	this.Determiner = determiner;
+	this.Pronoun = pronoun;
+	this.Reflexive = reflexives
+}
