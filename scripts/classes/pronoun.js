419cc77bb5c86b3de70c91edb7577fefe889c12d
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-25 18:51:55 +0000

    Removed un-needed folder

--- scripts/classes/pronoun.js
+++
@@ -1,8 +0,0 @@
-//Created by Tom Peregrine 03/09/2015 DD/MM/YYYY
-function Pronoun (nominative, objective, determiner, pronoun, reflexive) {
-	this.Nominative = nominative;
-	this.Objective = objective;
-	this.Determiner = determiner;
-	this.Pronoun = pronoun;
-	this.Reflexive = reflexives
-}
