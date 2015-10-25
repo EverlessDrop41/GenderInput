1277163a53fccb9af7798e6977d685f5dc1a1f30
Emily Peregrine <tomfperegrine@googlemail.com>
2015-10-25 17:14:16 +0000

    Setup basic gender prototypr

--- scripts/gender.js
+++ scripts/gender.js
@@ -1,27 +1,31 @@
 //GenderJS by Tom Peregrine
 //Started 10/08/2015 DD/MM/YYYY
-var FeminineMsg = "Female";
-var MasculineMsg = "Male"; 
-var NeutralMsg = "Agender";
-var FluidMsg = "Gender Fluid";
-var UnsureMsg = "Prefer not to say";
+function Gender(value) {
+	this.val = value;
+}
 
-function GetGenderStringFromVal (val) {
+Gender.FeminineMsg = "Female";
+Gender.MasculineMsg = "Male"; 
+Gender.NeutralMsg = "Gender Neutral";
+Gender.FluidMsg = "Gender Fluid";
+Gender.UnsureMsg = "Prefer not to say";
+
+Gender.prototype.toString = function () {
 	var outputMsg;
-	if (val >= 0 && val < 3.5) {
-		outputMsg = FeminineMsg;
+	if (this.val >= 0 && this.val < 3.5) {
+		outputMsg = Gender.FeminineMsg;
 	}
-	else if (val >= 3.5 && val < 6.5) {
-		outputMsg = NeutralMsg;
+	else if (this.val >= 3.5 && this.val < 6.5) {
+		outputMsg = Gender.NeutralMsg;
 	}
-	else if (val >= 6.5 && val <= 10) {
-		outputMsg = MasculineMsg;
+	else if (this.val >= 6.5 && this.val <= 10) {
+		outputMsg = Gender.MasculineMsg;
 	}
-	else if (val > 10) {
-		outputMsg = FluidMsg;
+	else if (this.val > 10) {
+		outputMsg = Gender.FluidMsg;
 	}
 	else {
-		outputMsg = UnsureMsg;
+		outputMsg = Gender.UnsureMsg;
 	}
 	return outputMsg;
 }
